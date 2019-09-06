8f8dedbd3509ec679acaac829ab58147890f25c6
cronwel <noelirias@gmail.com>
2019-09-06 15:41:27 -0500

    Blog scaffold created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :articles
+  get 'main/home'
+  get 'main/about'
+  get 'main/contact'
+
+  root "main#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
